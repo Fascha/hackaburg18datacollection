c4e4b9c05e447fc7b1fb89c64c2440e6c356cb5f
Fascha <fabian.schatz@googlemail.com>
2018-05-27 00:12:43 +0200

    let's go boyz

--- fifawm/tournament/simulate_tournament.py
+++ fifawm/tournament/simulate_tournament.py
@@ -3,6 +3,7 @@ from itertools import chain, groupby
 import csv
 import numpy
 import os
+import pandas as pd
 
 class Tournament(object):
 
@@ -85,8 +86,10 @@ class Tournament(object):
         records = []
         for k, g in groupby(sorted(groupperfs, key=lambda x: x.name), key=lambda x: x.name):
             records.append(self.getTeamRecord(k, g))
+
         qualified = {}
         group_standings = {}
+        group_table_total = {}
         for k, g in groupby(sorted(records, key=lambda x: x.group), key=lambda x: x.group):
             grouptable = sorted(g, key=lambda x: 1000000*x.p + 10000*(x.gs-x.ga) + 100*(x.gs), reverse=True)
             qualified['1' + k] = grouptable[0].name
@@ -96,7 +99,18 @@ class Tournament(object):
             group_standings['2' + k] = grouptable[1].name
             group_standings['3' + k] = grouptable[2].name
             group_standings['4' + k] = grouptable[3].name
-        return qualified, group_standings, groupresults
+            
+            
+            
+            group_table_standings = []
+            
+            group_table_standings.append([grouptable[0].name, grouptable[0].gs, grouptable[0].ga, grouptable[0].p])
+            group_table_standings.append([grouptable[1].name, grouptable[1].gs, grouptable[1].ga, grouptable[1].p])
+            group_table_standings.append([grouptable[2].name, grouptable[2].gs, grouptable[2].ga, grouptable[2].p])
+            group_table_standings.append([grouptable[3].name, grouptable[3].gs, grouptable[3].ga, grouptable[3].p])
+            
+            group_table_total[grouptable[0].group] = group_table_standings
+        return qualified, group_standings, groupresults, group_table_total
 
 
     def runEliminationRound(self, round, prevround, schedule, predictions):
@@ -108,12 +122,40 @@ class Tournament(object):
                 else result.match.team2
             results['L' + result.match.gameId] = result.match.team1 if result.team1Score < result.team2Score \
                 else result.match.team2
-        return results
+        return results, roundresults
 
 
     def get_tournament_prediction(self):
-        results, group_standings, groupresults = self.runGroupStage(self.schedule, self.predictions, self.teams)
+        results, group_standings, groupresults, group_table_total = self.runGroupStage(self.schedule, self.predictions, self.teams)
+        group_winners = results
+        all_rounds = []
         for rnd in ['AF','QF','SF','FN']:
-            results = self.runEliminationRound(rnd, results, self.schedule, self.predictions)
+            results, roundresults = self.runEliminationRound(rnd, results, self.schedule, self.predictions)
+            all_rounds.append(roundresults)
 
-        return results
+
+        columns = ['gameID', 'date', 'team1', 'team2', 'group', 'result']
+        new_df = pd.DataFrame(columns=columns)
+        for match in groupresults:
+            new_df = new_df.append(pd.DataFrame([[match.match.gameId,
+                                                match.match.date, 
+                                                match.match.team1, 
+                                                match.match.team2, 
+                                                match.match.group, 
+                                                '{}-{}'.format(match.team1Score, 
+                                                                match.team2Score)]], 
+                                                columns=columns))
+            
+
+        for round in all_rounds:
+            for match in round:
+                new_df = new_df.append(pd.DataFrame([[match.match.gameId,
+                                                    match.match.date, 
+                                                    match.match.team1, 
+                                                    match.match.team2, 
+                                                    match.match.group, 
+                                                    '{}-{}'.format(match.team1Score, 
+                                                                    match.team2Score)]], 
+                                                    columns=columns))
+
+        return group_winners, new_df, group_table_total
